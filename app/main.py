51514d83357ab6214971b559e31bd4496dec3e57
soravang81 <soravang81@gmail.com>
2025-08-04 15:13:48 +0530

    deploy

--- app/main.py
+++ app/main.py
@@ -11,8 +11,10 @@ from app.api.v1 import (
 from fastapi.middleware.cors import CORSMiddleware
 import socketio
 from app.services.socket import sio
+import os
 
-app = FastAPI(title="Healiora API", version="1.0.0")
+app = FastAPI(title="Healiora API", version="1.0.0" , debug=True)
+
 sio_asgi_app = socketio.ASGIApp(socketio_server=sio, other_asgi_app=app)
 
 app.mount("/socket.io/", sio_asgi_app, name="socket.io")
@@ -35,3 +37,13 @@ app.include_router(socket_log.router, prefix="/api/v1/socket-logs", tags=["Socke
 @app.get("/")
 def read_root():
     return {"message": "Healiora API is running!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True
+    )
---
+++ app/start.py
@@ -0,0 +1,11 @@
+import uvicorn
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False  # Set to False for production
+    ) 
