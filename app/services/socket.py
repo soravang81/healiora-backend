1042a1a418846a9a7d6fd2753b093d9b3946052a
soravang81 <soravang81@gmail.com>
2025-09-02 13:23:55 +0530

    local backup

--- app/services/socket.py
+++ app/services/socket.py
@@ -13,7 +13,10 @@ from datetime import datetime
 
 # mgr = socketio.AsyncRedisManager(url="redis://localhost:6379/0") 
 sio = socketio.AsyncServer(
-    async_mode="asgi", cors_allowed_origins="*"
+    async_mode="asgi", 
+    cors_allowed_origins="*",
+    logger=True,
+    engineio_logger=True
 )
 
 # Store connected users: {user_id: {socket_id: str, role: str}}
